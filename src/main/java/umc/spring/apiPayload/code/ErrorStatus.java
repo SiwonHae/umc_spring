00a532b9a52a1c110bd756910e0d4c8f933fc3b9
JuHyuk Lim <nyclo0705@gmail.com>
2024-11-13 16:07:47 +0900

    feat: 워크북 8주차 본문 진행

--- src/main/java/umc/spring/apiPayload/code/ErrorStatus.java
+++ src/main/java/umc/spring/apiPayload/code/ErrorStatus.java
@@ -19,12 +19,15 @@ public enum ErrorStatus implements BaseErrorCode {
     _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
 
 
-    // 멤버 관려 에러
+    // 멤버 관련 에러
     MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST, "MEMBER4001", "사용자가 없습니다."),
     NICKNAME_NOT_EXIST(HttpStatus.BAD_REQUEST, "MEMBER4002", "닉네임은 필수 입니다."),
 
     // 예시,,,
-    ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "ARTICLE4001", "게시글이 없습니다.");
+    ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "ARTICLE4001", "게시글이 없습니다."),
+
+    // 푸드 카테고리 관련 에러
+    FOOD_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "FOOD_CATEGORY_4001", "존재하지 않는 카테고리입니다.");
 
 
     private final HttpStatus httpStatus;
--- src/main/java/umc/spring/service/TempService/TempQueryServiceImpl.java
+++ src/main/java/umc/spring/service/TempService/TempQueryServiceImpl.java
@@ -1,4 +1,4 @@
-package umc.spring.service.tempService;
+package umc.spring.service.TempService;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
